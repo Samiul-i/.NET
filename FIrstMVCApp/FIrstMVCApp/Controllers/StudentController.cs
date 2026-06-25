b08a67599c4e30f8b90b561777cec140b78910c2
Samiul Islam <23-51004-1@student.aiub.edu>
2026-06-25 13:01:55 +0600

    First Code

---
+++ FIrstMVCApp/FIrstMVCApp/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FIrstMVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Student List";
+            ViewBag.Message = "Welcome to the Student Module";
+
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.StudentID = id;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return Content("This is the student module.");
+        }
+    }
+}
---
+++ FormPractice/FormPractice/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FormPractice.Models
+{
+    public class LoginController : Controller
+    {
+        public required string Username {  get; set; }
+        public required string Password { get; set; }
+        
+    }
+}
